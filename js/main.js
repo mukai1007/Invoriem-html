5ee33ca4e342e1291561b617384ce3f4e3322ff0
Your Name <you@example.com>
2022-09-06 10:54:26 +0600

    first html

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        dots: false,
+        margin: 48,
+        autoWidth: true,
+        loop: true
+    });
+});
+
+// video
+
+const videoBtn = document.querySelector('#video__btn')
+const videoPicture = document.querySelector('.video__picture')
+const videoWrapper = document.querySelector('.video')
+const video = document.querySelector('#video-file')
+
+videoWrapper.addEventListener('click', function () {
+    if (video.paused) {
+        videoPicture.classList.add('none')
+        videoWrapper.classList.remove('video-overlay')
+        videoBtn.classList.add('none')
+        video.play()
+    } else {
+        videoWrapper.classList.add('video-overlay')
+        videoBtn.classList.remove('none')
+        video.pause()
+    }
+
+})
